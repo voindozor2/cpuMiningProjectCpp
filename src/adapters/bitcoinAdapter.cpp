77867d4c268a8c5f71d9747838c1bc765debb1fd
Ильин Борис <boris.i.ilin@gazprombank.ru>
2025-11-02 11:18:45 +0300

    Добавлена реализация универсального адаптера

--- src/adapters/bitcoinAdapter.cpp
+++ src/adapters/bitcoinAdapter.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "bitcoinAdapter.h"
+#include <string>
+#include <cpr/cpr.h>
+
+bitcoinAdapter::bitcoinAdapter()
+{
+    this->host_ = "";
+    this->port_ = "";
+    this->user_ =  "";
+    this->password_ = "";
+}
+
+bitcoinAdapter::bitcoinAdapter(std::string host,std::string port,std::string user,std::string password)
+{
+    this->host_ = host;
+    this->port_ = port;
+    this->user_ =  user;
+    this->password_ = password;
+}
+
+std::string bitcoinAdapter::send(std::string contentType,std::string body)
+{
+    auto response = cpr::Post(
+        cpr::Url{"http://"+host_+ ":" + port_},
+        cpr::Authentication{user_, password_,cpr::AuthMode::BASIC},
+        cpr::Body{body},
+        cpr::Header{{"content-type", contentType}});
+    return response.text;
+}
--- src/adapters/bitcoinAdapter.h
+++ src/adapters/bitcoinAdapter.h
@@ -1,16 +1,17 @@
 //
 // Created by Boris on 02.11.2025.
 //
-
-#ifndef CPUMININGPROJECT_BITCOINADAPTER_H
-#define CPUMININGPROJECT_BITCOINADAPTER_H
-
-
+#include <string>
 
 class bitcoinAdapter {
-
+private:
+    std::string host_;
+    std::string port_;
+    std::string user_;
+    std::string password_;
+public:
+    bitcoinAdapter();
+    bitcoinAdapter(std::string host,std::string port,std::string user,std::string password);
+    ~bitcoinAdapter() = default;
+    std::string send(std::string contentType,std::string body);
 };
-
-
-
-#endif //CPUMININGPROJECT_BITCOINADAPTER_H
